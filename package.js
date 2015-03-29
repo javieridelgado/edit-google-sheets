dc9569b785b6042daaaa0d507d07d6ce44293608
Javier Delgado <javier.i.delgado@gmail.com>
2015-03-29 09:06:58 +0200

    Added sync versions of GoogleSheet.load and GoogleSheet.create.

--- package.js
+++ package.js
@@ -1,11 +1,8 @@
 Package.describe({
     name: 'jidelgado:edit-google-sheets',
-    version: '0.0.1',
+    version: '0.0.2',
     summary: 'Meteor wrapper for the edit-google-sheet npm package',
-    // URL to the Git repository containing the source code for this package.
-    git: '',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
+    git: 'https://github.com/javieridelgado/edit-google-sheets',
     documentation: 'README.md'
 });
 
